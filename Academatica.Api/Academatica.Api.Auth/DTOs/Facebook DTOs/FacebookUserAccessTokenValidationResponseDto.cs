af23cc6ab7d87c80879041e0bc3e4f5f0709b31f
Vladislav Pendishchuk <vmpendischuk@edu.hse.ru>
2022-01-14 21:41:08 +0300

    Current API project deleted

--- Academatica.Api/Academatica.Api.Auth/DTOs/Facebook DTOs/FacebookUserAccessTokenValidationResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using SmartMath.Api.Auth.Models;
-
-namespace SmartMath.Api.Auth.DTOs
-{
-    internal class FacebookUserAccessTokenValidationResponseDto
-    {
-        public FacebookUserAccessToken Data { get; set; }
-    }
-}
